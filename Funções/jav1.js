51911f26edc8b22114126ea81d344e7fd4c617e0
Pedro Almeida <luizapedroa@gmail.com>
2026-09-24 15:33:20 -0300

    Add files via upload

---
+++ Funções/jav1.js
@@ -0,0 +1,25 @@
+/*Crie uma função chamada calcularAreaRetangulo que receba dois parâmetros:
+base e altura. A função deve calcular e retornar o valor da área (área = base ×
+altura).*/
+/*Depois de uma explicação do professor eu entendi tudo, entendi que devia separar as funções para facilitar o serviço, estava com muita duvida sobre o funcionamento dos parâmetros e dos retornos mas depois de perceber que eu devia separar as funções retornar transformando a função no valor e igualar a variaveis separadas e então peço a resposta que é igualada a area achei dificil mas consegui.*/
+function lealtura(){
+    let altura = Number(prompt("Qual a altura do retângulo: "))
+    return altura
+}
+function lebase(){
+    let base = Number(prompt("Qual o tamanho da base do retângulo: "))
+    return base
+}
+function calcularAreaRetangulo(base, altura){
+    let area = a * b
+    return area
+}
+function imprimeResposta(r){
+    console.log(resposta)
+}
+let a = lealtura()
+let b = lebase()
+let resposta = calcularAreaRetangulo()
+imprimeResposta()
+
+
