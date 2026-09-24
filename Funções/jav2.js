e27d93f556f0913815459dd74b969208616da0cc
Pedro Almeida <luizapedroa@gmail.com>
2026-09-24 15:33:43 -0300

    Update jav2.js

--- Funções/jav2.js
+++ Funções/jav2.js
@@ -1,13 +1,27 @@
 /*Escreva uma função chamada ehPar que receba um número como parâmetro e
 retorne true se o número for par e false caso seja ímpar*/
-/*A Atividade foi facil apenas precisava criar a funçao que pedisse um número  e verificasse usando % 2 == 0 se era par então é imprimido true se não false.*/
-function ehPar(){
-    var numero = Number(prompt("Digite um número: "))
+/**/
+
+function LeNumero(){
+        let numero = Number(prompt("Digite um número: "))
+        return numero
+}
+
+function ehPar(numero){
     if(numero % 2 == 0){
         return true
     }else{
         return false
     }
 }
-console.log(ehPar())
+
+function imprimeResposta(r){
+    if(r == true){
+        alert("o numero eh par")
+    }
+}
+
+let n = LeNumero()
+let resposta = ehPar(n)
+imprimeResposta(resposta)
 
