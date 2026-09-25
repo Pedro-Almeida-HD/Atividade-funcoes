d3c45bb459117519e6a06e0af7af8274e348c92a
Pedro Almeida <luizapedroa@gmail.com>
2026-09-25 14:41:24 -0300

    Add files via upload

--- Funções/jav3.js
+++ Funções/jav3.js
@@ -1,9 +1,18 @@
 /*Crie uma função chamada celsiusParaFahrenheit que receba uma temperatura
 em Celsius e retorne a conversão para Fahrenheit (F = (C × 1.8) + 32). */
-/*Achei facil apenas tive que criar uma função que quando for chamada peça o valor em celsius e armazene em uma variavel depois em outra variavel calculo o valor na formula e returno fahrenheit.*/
-function celsiusParaFahrenheit(){
-    let celsius = Number(prompt("Qual a temperatura em celsius ? "))
-    let Fahrenheit = (celsius * 1.8) + 32
-    return Fahrenheit
+/*Eu achei a questão mediana pois ainda estou entendendo como funciona as funções ainda possuo dúvidas sobre o funcionamento dos parâmetros mas consegui fazer, pois so era necessário pedir o valor com a função lecelsius depois retornar e calcular para farenheit então retornar e imprimir a resposta em outra função*/
+function lecelsius(){
+    let c = Number(prompt("Qual a temperatura em celsius ? "))
+    return c
 }
-console.log(celsiusParaFahrenheit())
+function celsiusParaFahrenheit(celsius){
+    let f = (celsius * 1.8) + 32
+    return f
+}
+function imprimirResposta(Fahrenheit){
+    alert(Fahrenheit)
+}
+let celsius = lecelsius()
+let Fahrenheit = celsiusParaFahrenheit(celsius)
+imprimirResposta(Fahrenheit)
+
